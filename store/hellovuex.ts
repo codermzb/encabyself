bbafc105f1671335b030852de72c06cd515fac05
codermzb <1072192327@qq.com>
2020-03-31 08:11:31 +0800

    'hehe'

--- store/hellovuex.ts
+++ store/hellovuex.ts
@@ -1,18 +1,25 @@
-// import {  getModule, Module, VuexModule, Action, Mutation} from "vuex-module-decorators";
+// import { getModule, VuexModule, Module, Action, Mutation } from 'vuex-module-decorators'
 // import Vue from 'vue';
 // import Vuex from 'vuex';
 // Vue.use(Vuex);
 // const store = new Vuex.Store({})
 // @Module({dynamic: true, name: 'hello', store})
 // class Hello extends VuexModule {
-//   name = 'mzb';
+//   private name = 'mzb';
 //   get getName() {
-//     // console.log('ooo');
 //     return this.name;
 //   }
 //   @Mutation
 //   setName(name: string) {
 //     this.name = name;
 //   }
+//   @Action //不管有没有返回值,返回的都是一个Promise.
+//   value() {
+//     return new Promise((resolve, reject) => {
+//       console.log('开始设置name'); //只要调用,这两行必然执行.
+//       this.setName('dxz');
+//       resolve(this.getName); //调用结果成功
+//     })
+//   }
 // }
-// export const hello = getModule(Hello)
+// export const hellovuex = getModule(Hello);
